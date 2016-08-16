dd25edf185c7a2a5d8bf3c92c0c3b64a19a7cc60
naonaorange <nao.lunalovegood@gmail.com>
2016-08-16 17:16:18 +0900

    add sleep func

--- init-setup.sh
+++ init-setup.sh
@@ -60,8 +60,11 @@ read TMP
 if [ $TMP == "y" -o $TMP == "yes" ]
 then
   wget -q https://www.ubuntulinux.jp/ubuntu-ja-archive-keyring.gpg -O- | sudo apt-key add -
+  sleep 1s
   wget -q https://www.ubuntulinux.jp/ubuntu-jp-ppa-keyring.gpg -O- | sudo apt-key add -
+  sleep 1s
   sudo wget https://www.ubuntulinux.jp/sources.list.d/xenial.list -O /etc/apt/sources.list.d/ubuntu-ja.list
+  sleep 1s
   askIfInstall ubuntu-default-ja
 fi
 
